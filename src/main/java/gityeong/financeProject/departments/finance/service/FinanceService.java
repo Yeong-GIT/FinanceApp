5e9aa4167349cccf162f603ec33fcfebab930625
Admin-HL <yeonghaoliang0822@gmail.com>
2024-05-25 23:25:29 +0800

    Finance findAll method

---
+++ src/main/java/gityeong/financeProject/departments/finance/service/FinanceService.java
@@ -0,0 +1,9 @@
+package gityeong.financeProject.departments.finance.service;
+
+import gityeong.financeProject.departments.finance.entity.Finance;
+
+import java.util.List;
+
+public interface FinanceService {
+    List<Finance> findAll();
+}
